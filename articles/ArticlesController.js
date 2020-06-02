b2b615596f8edcbe5916addab59aad9b835aca33
Piscinao <henhvj@gmail.com>
2020-06-02 00:34:27 -0300

    Basic Crud finished

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,13 +5,40 @@ const Article = require("./Article")
 const slugify = require("slugify");
 
 router.get("/admin/articles", (req, res) => {
-    res.send("Router of articles")
+    Article.findAll({
+      // include date of category because relationship
+      include: [{model: Category}]
+    }).then(articles => {
+      res.render("admin/articles/index", {articles: articles})
+    });
+
 });
 
 router.get("/admin/articles/new", (req, res) => {
   Category.findAll().then(categories =>{
     res.render("admin/articles/new", {categories: categories})
   })
+  
+});
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+  var id = req.params.id;
+
+  if(isNaN(id)){
+    res.redirect("/admin/articles");
+  }
+
+ Article.findByPk(id).then(article => {
+   if(article != undefined){
+
+    res.render("admin/articles/edit", {article: article});
+
+   }else{
+     res.redirect("/admin/articles");
+   }
+ }).catch(erro => {
+   res.redirect("/admin/articles");
+ })
   
 });
 
@@ -27,6 +54,44 @@ router.post("/articles/save", (req, res) => {
     body: body,
     categoryId: category
   }).then(() =>{
+    res.redirect("/admin/articles");
+  })
+});
+
+router.post("/articles/delete", (req, res) => {
+  var id = req.body.id;
+
+  if(id != undefined){
+    // Verify if id is numeric
+    if(!isNaN(id)){
+      // Delete same upper id
+      Article.destroy({
+        where: {
+          id: id
+        }
+      }).then(() => {
+        res.redirect("/admin/articles");
+      })
+    } else{ // NOT A NUMBER
+      res.redirect("/admin/articles");
+    }
+
+  }else{ // NOT A NUMBER
+    res.redirect("/admin/articles");
+  }
+
+ });
+
+
+router.post("/articles/update", (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+
+  Article.update({title: title, slug: slugify(title) } , {
+    where:{
+      id: id
+    }
+  }).then(() => {
     res.redirect("/admin/articles");
   })
 });
