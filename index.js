118e835504247c7b1a92e82d40b3225eeaa8d2e8
Piscinao <henhvj@gmail.com>
2020-07-21 13:27:42 -0300

    update

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const articlesController = require("./articles/ArticlesController");
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
 
-
-
 //View engine
 app.set('view engine', 'ejs');
 
@@ -40,9 +38,34 @@ app.use("/", categoriesController);
 app.use("/", articlesController);
 
 app.get("/", (req, res) => {
-  res.render("index");
 
+  Article.findAll({
+    order:[
+      ['id', 'DESC']
+    ]
+  }).then(articles => {
+    res.render("index", {articles: articles});
+  });
 });
+
+app.get("/:slug", (req, res) => {
+  var slug = req.params.slug;
+  Article.findOne({
+    where: {
+      slug: slug
+    }
+  }).then(article => {
+    if(article != undefined){
+      res.render("article", {article: article});
+    }else{
+      res.redirect("/");
+
+      }
+    }).catch(err => {
+      res.redirect("/");
+    });
+  })
+
 
 app.listen(8090, () => {
   console.log("Server is running!");
